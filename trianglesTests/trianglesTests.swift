a47d70a136a5b185849ecc6bd8852a0ca273a610
TheLox <mirlan280306@gmail.com>
2024-11-12 13:26:04 +0300

    Initial Commit

---
+++ trianglesTests/trianglesTests.swift
@@ -0,0 +1,17 @@
+//
+//  trianglesTests.swift
+//  trianglesTests
+//
+//  Created by Мирлан Каныбеков on 12.11.2024.
+//
+
+import Testing
+@testable import triangles
+
+struct trianglesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
